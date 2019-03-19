0f327a9dca888cc184b3d5a63fdfb0fd0e3d160f
niuniu1122 <48205078+niuniu1122@users.noreply.github.com>
2019-03-19 21:40:30 +0800

    Add files via upload

--- sun/sun-service/src/main/java/edu/zut/cs/software/sun/admin/service/UserManager.java
+++ sun/sun-service/src/main/java/edu/zut/cs/software/sun/admin/service/UserManager.java
@@ -1,12 +1,6 @@
 package edu.zut.cs.software.sun.admin.service;
 
-import java.util.List;
-
-import edu.zut.cs.software.sun.admin.entity.User;
-
-
-
 public interface UserManager {
-public String sayHello(String name);
-public List<User> getAll();
+
+		String sayHello(String name);
 }
--- sun/sun-service/src/test/java/edu/zut/cs/software/sun/admin/service/UserManagerTest.java
+++ sun/sun-service/src/test/java/edu/zut/cs/software/sun/admin/service/UserManagerTest.java
@@ -1,10 +1,47 @@
 package edu.zut.cs.software.sun.admin.service;
 
-public class UserManagerTest {
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+
+@ContextConfiguration(locations = { "classpath:applicationContextTest-service.xml" })
+public class UserManagerTest extends AbstractJUnit4SpringContextTests {
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = LogManager.getLogger(UserManagerTest.class.getName());
+
+	/**
+	 * Logger for this class
+	 */
+	
+	UserManager userManager;
+
+	@Autowired
+	public void setUserManager(UserManager userManager) {
+		this.userManager = userManager;
+	}
+
+	@Test
+	public void testSayHello() {
+	
+
+		String expected = "Hello, world!";
+		String result = this.userManager.sayHello("world");
+		assertEquals(expected, result);
+
+		if (logger.isInfoEnabled()) {
+			logger.info("testSayHello() - String result={}", result); //$NON-NLS-1$
+		}
+		
 	}
 
 }
